c957e4f8197997823d2069bce8bcbf0489620c80
letthai <toitenthai250802@gmail.com>
2023-08-01 12:43:24 +0700

    create tetromino and action for it

--- TetrisUpdate/TetrisUpdate/main.cpp
+++ TetrisUpdate/TetrisUpdate/main.cpp
@@ -1,6 +1,5 @@
 #include "Header.h"
 #include "screen.h";
-#include "tetris.h"
 #include "window.h"
 #include "handlemedia.h"
 #include "media.h"
@@ -10,11 +9,11 @@
 //quan ly vung choi
 const int height = 20;
 const int width = 10;
-const int SQUARE_SIZE = 40;
+const int SQUARE_SIZE = 32;
 
 int field[height][width] = { 0 };
 
-struct position {
+struct Position {
 	int x;
 	int y;
 }
@@ -27,33 +26,33 @@ void draw(SDL_Renderer* renderer, SDL_Texture* texture[], int n);
 //ve cac khoi
 void draw(SDL_Renderer* renderer, SDL_Texture* texture[], int n) {
 	srand(static_cast<unsigned int>(time(0)));
-
-	for (int i = 0; i < 4; i++) {
-		shape1[i].x = (block[n][i] % 2) * SQUARE_SIZE + 5;
-		shape1[i].y = (block[n][i] / 2) * SQUARE_SIZE + 5;
+	if (shape1[0].x == 0) {
+		for (int i = 0; i < 4; i++) {
+				shape1[i].x = (block[n][i] % 2);
+				shape1[i].y = (block[n][i] / 2);
+			}
 	}
+	
 	for (int i = 0; i < 4; i++) {
 		int x = rand() % 6;
-		drawTexture(renderer, texture[1], shape1[i].x, shape1[i].y);
+		drawTexture(renderer, texture[x], shape1[i].x * SQUARE_SIZE, shape1[i].y * SQUARE_SIZE);
 	}
 }
 
 void drawTexture(SDL_Renderer* renderer, SDL_Texture* texture, int x, int y) {
 	SDL_Rect destRect = { x, y, SQUARE_SIZE, SQUARE_SIZE };
 	SDL_RenderCopy(renderer, texture, NULL, &destRect);
-	SDL_RenderPresent(renderer);
 }
 
 int main(int argc, char* args[])
 {
 	Window window("Super Tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	Media media;
 
 	SDL_Window* sdlwindow = window.getWindow();
 	SDL_Renderer* sdlrender = window.getRenderer();
 	SDL_Surface* sdlscreen = window.getTetrisScreen();
-	SDL_Surface* sdlbackground = media.getTetrisbackground();
-	SDL_Texture* sdltexture = media.getTetrisTexture();
+	SDL_Surface* sdlbackground = loadSurface(sdlscreen, "image/background/space.png");
+	SDL_Texture* sdltexture = SDL_CreateTextureFromSurface(sdlrender, sdlbackground);
 
 	SDL_Texture* list_texture[] = {
 		loadTexture(sdlrender, "image/block/planet_1.png"),
@@ -77,29 +76,71 @@ int main(int argc, char* args[])
 		}
 		else
 		{
-			//Main loop flag
-			bool quit = false;
-
-			//Event handler
+			bool isPlaying = true;
 			SDL_Event e;
-
+			int delta = 0;
+			bool isRotate = false;
 			//While application is running
-			while (!quit)
+			while (isPlaying)
 			{
 				//Handle events on queue
 				while (SDL_PollEvent(&e) != 0)
 				{
-					//User requests quit
-					if (e.type == SDL_QUIT)
+					switch (e.type)
 					{
-						quit = true;
+					case SDL_QUIT:
+						isPlaying = false;
+					case SDL_KEYDOWN:
+						switch (e.key.keysym.sym)
+						{
+						case SDLK_UP:
+							isRotate = true;
+							break;
+						case SDLK_DOWN:
+							break;
+						case SDLK_LEFT:
+							delta = -1;
+							break;
+						case SDLK_RIGHT:
+							delta = 1;
+							break;
+						default:
+							break;
+						}
+					default:
+						break;
 					}
 				}
-				//applySurface(sdlbackground, sdlscreen, sdlwindow, "image/background/space.jpg");
-				applyTexture(sdlrender, sdltexture, "image/background/space.jpg");
-	draw(sdlrender, list_texture, 1);
-				
-				//applyTexture(sdlrender, sdltexture, "image/block/planet-1.png");
+
+				for (int i = 0; i < 4; i++) {
+					shape1[i].x += delta;
+				}
+
+				if (isRotate) {
+					Position point = shape1[2];
+					for (int i = 0; i < 4; i++) {
+						int a = shape1[i].y - point.y;
+						int b = shape1[i].x - point.x;
+						shape1[i].x = point.x - a;
+						shape1[i].y = point.y + b;
+ 					}
+				}
+
+				SDL_SetRenderDrawColor(sdlrender, 0, 0, 0, 255);
+				SDL_RenderClear(sdlrender);
+
+				// Draw background
+				SDL_RenderCopy(sdlrender, sdltexture, NULL, NULL);
+
+				// Draw the tetromino
+				draw(sdlrender, list_texture, 1);
+
+				// Reset the variable of action
+				delta = 0;
+				isRotate = false;
+
+				// Update the screen
+				SDL_RenderPresent(sdlrender);
 			}
 		}
 	}
