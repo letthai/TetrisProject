fc6c809c1ec668f3f8b3d79415e3ccddf1899057
letthai <toitenthai250802@gmail.com>
2023-08-05 00:57:47 +0700

    update stop when reach the limit

--- TetrisUpdate/TetrisUpdate/main.cpp
+++ TetrisUpdate/TetrisUpdate/main.cpp
@@ -25,9 +25,52 @@ bool check() {
 	return true;
 }
 
+void drawBlock();
+void drawBlock() {
+	for (int i = 0; i < 4; i++) {
+		int randomNumber = rand() % 6;
+		colorOfBlock[i] = randomNumber;
+	}
+	int n = rand() % 7;
+	for (int i = 0; i < 4; i++) {
+		shape1[i].x = (block[n][i] % 2);
+		shape1[i].y = (block[n][i] / 2) - 1;
+	}
+}
+int countNumberOfSameBlock(Position shape);
+
+int countNumberOfSameBlock(Position shape) {
+	int count = 0;
+	if (field[shape.y][shape.x]){
+		if (field[shape.y][shape.x] == field[shape.y - 1][shape.x]) {
+			count++;
+			if (field[shape.y - 1][shape.x] == field[shape.y - 2][shape.x]) count++;
+			if (field[shape.y - 1][shape.x] == field[shape.y - 1][shape.x - 1]) count++;
+			if (field[shape.y - 1][shape.x] == field[shape.y - 1][shape.x + 1]) count++;
+		}
+		if (field[shape.y][shape.x] == field[shape.y + 1][shape.x]) {
+			count++;
+			if (field[shape.y + 1][shape.x] == field[shape.y + 2][shape.x]) count++;
+			if (field[shape.y + 1][shape.x] == field[shape.y + 1][shape.x - 1]) count++;
+			if (field[shape.y + 1][shape.x] == field[shape.y + 1][shape.x + 1]) count++;
+		}
+		if (field[shape.y][shape.x] == field[shape.y][shape.x + 1]) {
+			count++;
+			if (field[shape.y][shape.x + 1] == field[shape.y][shape.x + 2]) count++;
+ 		}
+		if (field[shape.y][shape.x] == field[shape.y][shape.x - 1]) {
+			count++;
+			if (field[shape.y][shape.x - 1] == field[shape.y][shape.x - 2]) count++;
+		}
+		return count;
+	}
+}
+
+
 int main(int argc, char* args[])
 {
 	srand(time(0));
+	drawBlock();
 
 	Window window("Super Tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	SDL_Renderer* sdlrender = window.getRenderer();
@@ -59,8 +102,10 @@ int main(int argc, char* args[])
 			SDL_Event e;
 			int delta = 0;
 			bool isRotate = false;
+			bool gameOver = false;
 			int delay = 300;
 			Uint32 startTime = 0;
+			int countBlocks = 0;
 			//While application is running
 			while (isPlaying)
 			{
@@ -97,72 +142,81 @@ int main(int argc, char* args[])
 					}
 				}
 
-				// Move tetromino to the left or right
-				for (int i = 0; i < 4; i++) {
-					shape2[i] = shape1[i];
-					shape1[i].x += delta;
-				}
-				if (!check()) {
-					for (int i = 0; i < 4; i++) shape1[i] = shape2[i];
-				}
-
-				// Rotate the tetromino
-				if (isRotate) {
-					Position point = shape1[1];
-					for (int i = 0; i < 4; i++) {
-						int a = shape1[i].y - point.y;
-						int b = shape1[i].x - point.x;
-						shape1[i].x = point.x - a;
-						shape1[i].y = point.y + b;
-					}
-					if (!check()) for (int i = 0; i < 4; i++) shape1[i] = shape2[i];
-				}
-
-				// Run down the tetromino faster and create new tetromino
-				if (currentTime - startTime > delay) {
+				if (!gameOver) {
+					// Move tetromino to the left or right
 					for (int i = 0; i < 4; i++) {
 						shape2[i] = shape1[i];
-						shape1[i].y++;
+						shape1[i].x += delta;
 					}
 					if (!check()) {
-						for (int i = 0; i < 4; i++) {
-							field[shape2[i].y][shape2[i].x] = 1 + colorOfBlock[i];
-						}
-						for (int i = 0; i < 4; i++) {
-							int randomNumber = rand() % 6;
-							colorOfBlock[i] = randomNumber;
-						}
-						int n = rand() % 7;
-						for (int i = 0; i < 4; i++) {
-							shape1[i].x = (block[n][i] % 2);
-							shape1[i].y = (block[n][i] / 2);
-						}
+						for (int i = 0; i < 4; i++) shape1[i] = shape2[i];
 					}
-					startTime = currentTime;
-				}
 
-				// Check line if have all block in this line
-				int line = height - 1;
-				for (int i = height - 1; i > 0; i--)
-				{
-					int count = 0;
-					for (int j = 0; j < width; j++)
+					// Rotate the tetromino
+					if (isRotate) {
+						Position point = shape1[1];
+						for (int i = 0; i < 4; i++) {
+							int a = shape1[i].y - point.y;
+							int b = shape1[i].x - point.x;
+							shape1[i].x = point.x - a;
+							shape1[i].y = point.y + b;
+						}
+						if (!check()) for (int i = 0; i < 4; i++) shape1[i] = shape2[i];
+					}
+
+					// Run down the tetromino faster and create new tetromino
+					if (currentTime - startTime > delay) {
+						for (int i = 0; i < 4; i++) {
+							shape2[i] = shape1[i];
+							shape1[i].y++;
+						}
+						if (!check()) {
+							// Can hieu code da
+							if (gameOver) {
+								// Find the top limit (highest row) of the last tetromino
+								int topLimit = height;
+								for (int i = 0; i < 4; i++) {
+									if (shape2[i].y < topLimit) {
+										topLimit = shape2[i].y;
+									}
+								}
+
+								// Adjust the y-coordinate of the last tetromino to fit within the play area
+								for (int i = 0; i < 4; i++) {
+									shape2[i].y -= (shape2[i].y - topLimit);
+								}
+							}
+							for (int i = 0; i < 4; i++) {
+								field[shape2[i].y][shape2[i].x] = 1 + colorOfBlock[i];
+							}
+							drawBlock();
+						}
+						startTime = currentTime;
+					}
+
+					// Check line if have all block in this line
+					int line = height - 1;
+					for (int i = line; i >= 0; i--)
 					{
-						if (field[i][j]) count++;
-						field[line][j] = field[i][j];
+						int count = 0;
+						for (int j = 0; j < width; j++)
+						{
+							if (field[i][j]) {
+								if (i == 0) {
+									gameOver = true;
+									break;
+								}
+								count++;
+							}
+							field[line][j] = field[i][j];
+						}
+						if (count < width) line--;
 					}
-					if (count < width) line--;
 				}
-
-				// Check group of the block if have same texture
-				/*int count = 0;
-				for (int i = 0; i < 4; i++) {
-					if()
-				}*/
-
 				delta = 0;
 				isRotate = false;
 				delay = 300;
+				countBlocks = 0;
 
 				SDL_RenderClear(sdlrender);
 
@@ -171,9 +225,12 @@ int main(int argc, char* args[])
 				// Render the tetromino when it stop
 				for (int i = 0; i < height; i++) {
 					for (int j = 0; j < width; j++) {
-						if (field[i][j]) {
+						if (field[i][j] == 0) continue;
+						else {
 							drawTexture(sdlrender, list_texture[field[i][j] - 1], j * SQUARE_SIZE, i * SQUARE_SIZE);
 						}
+						
+						
 					}
 				}
 
