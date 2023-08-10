7199369fd145fdc9b1d73d5f0a9de1055a0ff9ac
letthai <toitenthai250802@gmail.com>
2023-08-10 15:24:05 +0700

    add function in handlemedia file
    add createRect and ConvertTextToTexture function

--- TetrisUpdate/TetrisUpdate/handlemedia.cpp
+++ TetrisUpdate/TetrisUpdate/handlemedia.cpp
@@ -12,6 +12,15 @@ void moveRect(SDL_Rect& rect, int x, int y) {
 	rect.y += y;
 }
 
+SDL_Rect createRect(int x, int y, int w, int h) {
+	SDL_Rect rect;
+	rect.x = x;
+	rect.y = y;
+	rect.w = w;
+	rect.h = h;
+	return rect;
+}
+
 void drawTexture(SDL_Renderer* renderer, SDL_Texture* texture, int x, int y) {
 	SDL_Rect destRect = { x, y, SQUARE_SIZE, SQUARE_SIZE };
 	moveRect(destRect, 341, 13);
@@ -89,3 +98,32 @@ void applySurface(SDL_Surface* src, SDL_Surface* des, SDL_Window* window, string
 	SDL_UpdateWindowSurface(window);
 }
 
+
+void converTextToTexture(SDL_Renderer* render ,string text, string filepath, int size, SDL_Color fg,
+								SDL_Color bg, int type, int x1, int y1, int x2, int y2) {
+	SDL_Surface* surface = NULL;
+	SDL_Texture* texture = NULL;
+	TTF_Font* font = TTF_OpenFont(filepath.c_str(), size);
+
+	switch (type)
+	{
+	case 1:
+		surface = TTF_RenderText_Shaded(font, text.c_str(), fg, bg);
+	case 2:
+		surface = TTF_RenderText_Solid(font, text.c_str(), fg);
+	case 3:
+		surface = TTF_RenderText_Blended(font, text.c_str(), fg);
+	default:
+		break;
+	}
+
+	texture = SDL_CreateTextureFromSurface(render, surface);
+	SDL_FreeSurface(surface);
+
+	int w, h;
+	TTF_SizeText(font, text.c_str(), &w, &h);
+	SDL_Rect srcRest = createRect(x1, y1, w, h);
+	SDL_Rect desRect = createRect(x2, y2, w, h);
+
+	SDL_RenderCopy(render, texture, &srcRest, &desRect);
+}
--- TetrisUpdate/TetrisUpdate/handlemedia.h
+++ TetrisUpdate/TetrisUpdate/handlemedia.h
@@ -12,6 +12,12 @@ SDL_Texture* loadTexture(SDL_Renderer* renderer, string path);
 SDL_Surface* loadSurface(SDL_Surface* screen, string path);
 void applySurface(SDL_Surface* src, SDL_Surface* des, SDL_Window* window, string filepath);
 
+// Xu ly hinh chu nhat
 void moveRect(SDL_Rect& rect, int x, int y);
+SDL_Rect createRect(int x, int y, int w, int h);
+
+// Xu ly text
+void converTextToTexture(SDL_Renderer* render, string text, string filepath, int size,
+								SDL_Color fg, SDL_Color bg, int type, int x1, int y1, int x2, int y2);
 #endif // !HANDLE_MEDIA_H_
 
