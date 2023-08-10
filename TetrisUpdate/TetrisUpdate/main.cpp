585c13b679abc919b37a1a022c38a16e1e0a7c1f
letthai <toitenthai250802@gmail.com>
2023-08-10 15:26:08 +0700

    add button in main
    add button and its function

--- TetrisUpdate/TetrisUpdate/main.cpp
+++ TetrisUpdate/TetrisUpdate/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include "Header.h"
 #include "window.h"
 #include "handlemedia.h"
+#include "button.h"
 
 #undef main
 
@@ -73,18 +74,6 @@ int main(int argc, char* args[])
 	srand(time(0));
 	drawBlock();
 
-	if (TTF_Init() < 0)
-	{
-		SDL_Log("%s", TTF_GetError());
-		return -1;
-	}
-	
-	TTF_Font* font = NULL;
-	SDL_Surface* surface = NULL;
-	SDL_Texture* texture = NULL;
-
-	SDL_Texture* textureframe = NULL;
-
 	Window window("Super Tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	SDL_Renderer* sdlrender = window.getRenderer();
 
@@ -97,38 +86,16 @@ int main(int argc, char* args[])
 		loadTexture(sdlrender, "image/block/planet_6.png"), 
 	};
 
+	Button button_start(sdlrender, "image/button/play_1.png", "image/button/play_2.png", 426, 175, 150, 70);
+	Button button_continue(sdlrender, "image/button/continue_1.png", "image/button/continue_2.png", 426, 265, 150, 70);
+	Button button_quit(sdlrender, "image/button/quit_1.png", "image/button/quit_2.png", 426, 355, 150, 70);
+	Button button_retry(sdlrender, "image/button/retry_1.png", "image/button/retry.png", 40, 108, 50, 50);
+	Button button_mainsc(sdlrender, "image/button/x_1.png", "image/button/x_2.png", 120, 108, 50, 50);
+	Button button_pause(sdlrender, "image/button/pause_1.png", "image/button/pause_2.png", 10, 10, 50, 50);
+	Button button_resume(sdlrender, "image/button/resume_1.png", "image/button/resume_2.png", 426, 175, 150, 70);
+	Button button_save(sdlrender, "image/button/save_1.png", "image/button/save_2.png", 426, 265, 150, 70);
 
-	font = TTF_OpenFont("font/VT323-Regular.ttf", 90);
-
-	SDL_Color fg = { 255, 255, 255 };
-
-	SDL_Color bg = { 0, 0, 0 };
-
-	string text = "GAME OVER";
-	surface = TTF_RenderText_Shaded(font, text.c_str(), fg, bg);
-	texture = SDL_CreateTextureFromSurface(sdlrender, surface);
-	SDL_FreeSurface(surface);
-
-	SDL_Rect srcRest;
-	SDL_Rect desRect;
-	SDL_Rect desRect1;
-	TTF_SizeText(font, text.c_str(), &srcRest.w, &srcRest.h);
-
-	srcRest.x = 0;
-	srcRest.y = 0;
-
-	desRect.x = 337;
-	desRect.y = 270;
-
-	desRect.w = srcRest.w;
-	desRect.h = srcRest.h;
-
-	desRect1.x = 223;
-	desRect1.y = 0;
-	
-	desRect1.w = 555;
-	desRect1.h = 667;
-
+	int hi = 0;
 	bool flags = true;
 	if (!window.init())
 	{
@@ -142,7 +109,9 @@ int main(int argc, char* args[])
 		}
 		else
 		{
+			// Khai bao cac bien
 			bool isPlaying = true;
+			bool isStarted = false;
 			int score = 0;
 			SDL_Event e;
 			int delta = 0;
@@ -151,7 +120,13 @@ int main(int argc, char* args[])
 			int delay = 300;
 			Uint32 startTime = 0;
 			int countBlocks = 0;
+			bool isPause = false;
 
+			SDL_Color fg = { 255, 255, 255 };
+			SDL_Color bg = { 0, 0, 0 };
+
+			// Render background
+			SDL_RenderCopy(sdlrender, window.getTetrisBackground(), NULL, NULL);
 			//While application is running
 			while (isPlaying)
 			{
@@ -182,112 +157,200 @@ int main(int argc, char* args[])
 						default:
 							break;
 						}
+					case SDL_MOUSEBUTTONDOWN:
+						button_start.checkEventPress(e);
+						button_continue.checkEventPress(e);
+						button_quit.checkEventPress(e);
+						button_retry.checkEventPress(e); 
+						button_mainsc.checkEventPress(e);
+						button_pause.checkEventPress(e); 
+						button_resume.checkEventPress(e);
+						button_save.checkEventPress(e);
+						if (button_quit.isPressed() == true) isPlaying = false;
+
+						// Check pause or resume game
+						if (button_pause.isPressed() == true && isPause == false) isPause = true;
+						else if (button_resume.isPressed() == true) {
+							if (isPause == true) isPause = false;
+						}
+
+						// Check if user start game
+						if (button_start.isPressed() == true && isStarted == false) {
+							isStarted = true;
+						}
+						else if (gameOver == true) {
+							// Retry game
+							if (button_retry.isPressed() == true) {
+								gameOver = false;
+								isStarted = true;
+								score = 0;
+								memset(field, 0, sizeof(field));
+								drawBlock();
+							}
+							// Back to main screen
+							else if (button_mainsc.isPressed() == true) {
+								gameOver = false;
+								isStarted = false;
+								score = 0;
+								memset(field, 0, sizeof(field));
+								drawBlock();
+							}
+						}
+					case SDL_MOUSEBUTTONUP:
+						button_start.checkEventPress(e);
+						button_continue.checkEventPress(e);
+						button_quit.checkEventPress(e);
+						button_retry.checkEventPress(e);
+						button_mainsc.checkEventPress(e);
+						button_pause.checkEventPress(e);
+						button_resume.checkEventPress(e);
+						button_save.checkEventPress(e);
+					case SDL_MOUSEMOTION:
+						button_start.checkEventPress(e);
+						button_continue.checkEventPress(e);
+						button_quit.checkEventPress(e);
+						button_retry.checkEventPress(e);
+						button_mainsc.checkEventPress(e);
+						button_pause.checkEventPress(e);
+						button_resume.checkEventPress(e);
+						button_save.checkEventPress(e);
 					default:
 						break;
 					}
 				}
 
-				if (!gameOver) {
-					// Move tetromino to the left or right
-					for (int i = 0; i < 4; i++) {
-						shape2[i] = shape1[i];
-						shape1[i].x += delta;
-					}
-					if (!check()) {
-						for (int i = 0; i < 4; i++) shape1[i] = shape2[i];
-					}
-
-					// Rotate the tetromino
-					if (isRotate) {
-						Position point = shape1[1];
-						for (int i = 0; i < 4; i++) {
-							int a = shape1[i].y - point.y;
-							int b = shape1[i].x - point.x;
-							shape1[i].x = point.x - a;
-							shape1[i].y = point.y + b;
-						}
-						if (!check()) for (int i = 0; i < 4; i++) shape1[i] = shape2[i];
-					}
-
-					// Run down the tetromino faster and create new tetromino
-					if (currentTime - startTime > delay) {
-						for (int i = 0; i < 4; i++) {
-							shape2[i] = shape1[i];
-							shape1[i].y++;
-						}
-						if (!check()) {
-							// Delete the block out of range
-							if (gameOver) {
-								int topLimit = height;
-								for (int i = 0; i < 4; i++) {
-									if (shape2[i].y < topLimit) {
-										topLimit = shape2[i].y;
-									}
-								}
-
-								for (int i = 0; i < 4; i++) {
-									shape2[i].y -= (shape2[i].y - topLimit);
-								}
-							}
-							for (int i = 0; i < 4; i++) {
-								field[shape2[i].y][shape2[i].x] = 1 + colorOfBlock[i];
-							}
-							drawBlock();
-						}
-						startTime = currentTime;
-					}
-
-					// Check line if have all block in this line
-					int line = height - 1;
-					for (int i = line; i >= 0; i--)
-					{
-						int count = 0;
-						for (int j = 0; j < width; j++)
-						{
-							if (field[i][j]) {
-								if (i == 0) {
-									gameOver = true;
-									break;
-								}
-								count++;
-							}
-							field[line][j] = field[i][j];
-						}
-						if (count < width) line--;
-					}
-				}
-
-				delta = 0;
-				isRotate = false;
-				delay = 300;
-				countBlocks = 0;
-
 				SDL_RenderClear(sdlrender);
 
+				// Render background
 				SDL_RenderCopy(sdlrender, window.getTetrisBackground(), NULL, NULL);
+				
+				if (isStarted == false) {
+					button_start.drawButton(sdlrender);
+					button_continue.drawButton(sdlrender);
+					button_quit.drawButton(sdlrender);
+				}
+				else {
+					if (!gameOver) {
+						if (isPause == false) button_pause.drawButton(sdlrender);
 
-				//SDL_RenderCopy(sdlrender, textureframe, NULL, &desRect1);
+						// Move tetromino to the left or right
+						for (int i = 0; i < 4; i++) {
+							shape2[i] = shape1[i];
+							shape1[i].x += delta;
+						}
+						if (!check()) {
+							for (int i = 0; i < 4; i++) shape1[i] = shape2[i];
+						}
 
-				// Render the tetromino when it stop
-				for (int i = 0; i < height; i++) {
-					for (int j = 0; j < width; j++) {
-						if (field[i][j] == 0) continue;
-						else {
-							drawTexture(sdlrender, list_texture[field[i][j] - 1], j * SQUARE_SIZE, i * SQUARE_SIZE);
+						// Rotate the tetromino
+						if (isRotate) {
+							Position point = shape1[1];
+							for (int i = 0; i < 4; i++) {
+								int a = shape1[i].y - point.y;
+								int b = shape1[i].x - point.x;
+								shape1[i].x = point.x - a;
+								shape1[i].y = point.y + b;
+							}
+							if (!check()) for (int i = 0; i < 4; i++) shape1[i] = shape2[i];
+						}
+
+						// Run down the tetromino faster and create new tetromino
+						if(isPause == false) {
+							if (currentTime - startTime > delay) {
+								for (int i = 0; i < 4; i++) {
+									shape2[i] = shape1[i];
+									shape1[i].y++;
+								}
+								if (!check()) {
+									// Delete the block out of range
+									if (gameOver) {
+										int topLimit = height;
+										for (int i = 0; i < 4; i++) {
+											if (shape2[i].y < topLimit) {
+												topLimit = shape2[i].y;
+											}
+										}
+
+										for (int i = 0; i < 4; i++) {
+											shape2[i].y -= (shape2[i].y - topLimit);
+										}
+									}
+									for (int i = 0; i < 4; i++) {
+										field[shape2[i].y][shape2[i].x] = 1 + colorOfBlock[i];
+									}
+									drawBlock();
+								}
+								startTime = currentTime;
+							}
+						}
+
+						// Check line if have all block in this line
+						int line = height - 1;
+						for (int i = line; i >= 0; i--)
+						{
+							int count = 0;
+							for (int j = 0; j < width; j++)
+							{
+								if (field[i][j]) {
+									if (i == 0) {
+										gameOver = true;
+										break;
+									}
+									count++;
+								}
+								field[line][j] = field[i][j];
+							}
+							if (count < width) line--;
+							else {
+								score += 100;
+							}
 						}
 					}
-				}
-				
-				// Render the tetromino when it is running down
-				for (int i = 0; i < 4; i++) {
-					drawTexture(sdlrender, list_texture[colorOfBlock[i]], shape1[i].x * SQUARE_SIZE, shape1[i].y * SQUARE_SIZE);
+
+					delta = 0;
+					isRotate = false;
+					delay = 300;
+					countBlocks = 0;
+
+					// Render the tetromino when it stop
+					for (int i = 0; i < height; i++) {
+						for (int j = 0; j < width; j++) {
+							if (field[i][j] == 0) continue;
+							else {
+								drawTexture(sdlrender, list_texture[field[i][j] - 1], j * SQUARE_SIZE, i * SQUARE_SIZE);
+							}
+						}
+					}
+
+					// Render the tetromino when it is running down
+					for (int i = 0; i < 4; i++) {
+						drawTexture(sdlrender, list_texture[colorOfBlock[i]], shape1[i].x * SQUARE_SIZE, shape1[i].y * SQUARE_SIZE);
+					}
+
+					if (isPause == true) {
+						button_resume.drawButton(sdlrender);
+						button_save.drawButton(sdlrender);
+						button_quit.drawButton(sdlrender);
+					}
+
+					if (gameOver == true) {
+						if (score >= hi) hi = score;
+						converTextToTexture(sdlrender, "Retry?", "font/VT323-Regular.ttf", 50, fg, bg, 1, 0, 0, 50, 50);
+						button_retry.drawButton(sdlrender);
+						button_mainsc.drawButton(sdlrender);
+						converTextToTexture(sdlrender, "GAME OVER", "font/VT323-Regular.ttf", 90, fg, bg, 1, 0, 0, 337, 270);
+
+						// In ra high score
+						converTextToTexture(sdlrender, to_string(hi), "font/VT323-Regular.ttf", 65, fg, bg, 1, 0, 0, 785, 200);
+					}
+
+					converTextToTexture(sdlrender, to_string(score), "font/VT323-Regular.ttf", 65, fg, bg, 1, 0, 0, 765, 115);
 				}
 
-				if (gameOver == true) {
-					SDL_RenderCopy(sdlrender, texture, &srcRest, &desRect);
-				}
-
+				//Render frame
+				SDL_Rect desRect1 = createRect(223, 0, 555, 667);
 				SDL_RenderCopy(sdlrender, window.getTetrisFrame(), NULL, &desRect1);
+
 				// Update the screen
 				SDL_RenderPresent(sdlrender);
 			}
@@ -299,105 +362,3 @@ int main(int argc, char* args[])
 
 	return 0;
 }
-
-//int main()
-//{
-//	SDL_Window* window = NULL;
-//	SDL_Renderer* renderer = NULL;
-//	bool isRunning = true;
-//	SDL_Event mainEvent;
-//	TTF_Font* font = NULL;
-//	SDL_Surface* surface = NULL;
-//	SDL_Texture* texture = NULL;
-//
-//	//initializes  the subsystems
-//	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-//	{
-//		printf("Unable to initialize SDL %s\n", SDL_GetError());
-//		return -1;
-//	}
-//
-//	//Initialize the truetype font API.
-//	if (TTF_Init() < 0)
-//	{
-//		SDL_Log("%s", TTF_GetError());
-//		return -1;
-//	}
-//
-//	//Create window
-//	window = SDL_CreateWindow("Stdio.vn - SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 640, SDL_WINDOW_SHOWN);
-//	if (window == NULL)
-//	{
-//		printf("Could not create window %s", SDL_GetError());
-//		return -1;
-//	}
-//
-//	//create a renderer
-//	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-//	if (renderer == NULL)
-//	{
-//		printf("Could not create render %s", SDL_GetError());
-//		return -1;
-//	}
-//
-//	font = TTF_OpenFont("font/VT323-Regular.ttf", 30);
-//
-//	SDL_Color fg = { 243, 156, 18 };
-//
-//	std::string text = "Welcome you to Stdio.vn";
-//	surface = TTF_RenderText_Solid(font, text.c_str(), fg);
-//	texture = SDL_CreateTextureFromSurface(renderer, surface);
-//	SDL_FreeSurface(surface);
-//
-//	SDL_Rect srcRest;
-//	SDL_Rect desRect;
-//	TTF_SizeText(font, text.c_str(), &srcRest.w, &srcRest.h);
-//
-//	srcRest.x = 0;
-//	srcRest.y = 0;
-//
-//	desRect.x = 200;
-//	desRect.y = 270;
-//
-//	desRect.w = srcRest.w;
-//	desRect.h = srcRest.h;
-//
-//	//set background color
-//	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-//	//main loop
-//	while (isRunning)
-//	{
-//		//main event
-//		while (SDL_PollEvent(&mainEvent))
-//		{
-//			switch (mainEvent.type)
-//			{
-//				//User - requested quit
-//			case SDL_QUIT:
-//			{
-//				isRunning = false;
-//				break;
-//			}
-//			default:
-//			{
-//				break;
-//			}
-//			}
-//		}
-//		// clear the window to black
-//		SDL_RenderClear(renderer);
-//		//Copy a portion of the texture to the current rendering target.
-//		SDL_RenderCopy(renderer, texture, &srcRest, &desRect);
-//		//draw to screen
-//		SDL_RenderPresent(renderer);
-//	}
-//	//Destroy a window.
-//	SDL_DestroyWindow(window);
-//	//Destroy a renderer
-//	SDL_DestroyRenderer(renderer);
-//	//Shutdown and cleanup the truetype font API.
-//	TTF_Quit();
-//	//cleans up all initialized subsystems
-//	SDL_Quit();
-//	return 0;
-//}
